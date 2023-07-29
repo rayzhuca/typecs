ec666092b4216de03107f860856726a67c1ec795
RayBipse <raybipse@gmail.com>
2023-07-28 21:13:19 -0700

    DB setup

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(
     __name__,
     template_folder="templates"
 )
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
