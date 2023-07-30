404b5ff447292aa06d3ea2d35686000ce72f7e12
RayBipse <raybipse@gmail.com>
2023-07-29 17:12:16 -0700

    Added HTML/CSS for left side of index.html

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ load_dotenv()
 
 app = Flask(
     __name__,
-    template_folder="templates"
+    template_folder="templates",
+    static_folder="statics"
 )
 app.config["SQLALCHEMY_DATABASE_URI"] =  os.getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
